d204b790d162011ae27776d1b815fe8d1da67087
zhil.shi <561120823@qq.com>
2015-07-11 12:40:16 +0800

    Initial Commit

---
+++ SZLPhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZLPhotoBrowser
+//
+//  Created by mr.shi on 15/7/11.
+//  Copyright (c) 2015年 zhil.shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SZLPhotoBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SZLPhotoBrowser
+//
+//  Created by mr.shi on 15/7/11.
+//  Copyright (c) 2015年 zhil.shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
